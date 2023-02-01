95c55e5de2ef611330f5db0e26cef420d1669101
Luis Carlos Castro Sandoval <102004697+luisxsssx@users.noreply.github.com>
2023-02-01 16:52:51 -0600

    Add files via upload

---
+++ JavaScript/Ejercicio2/Ejercicio1.js
@@ -0,0 +1,19 @@
+let name = 'Luis Carlos Castro Sandoval';
+let edad = 19;
+let eresDeveloper = true;
+var fechaDeNac = new Date(2003, 05, 21);
+
+var libroFavorito = {
+    titulo: "El nombre del viento",
+    autor: "Patrick Rothfuss",
+    fecha: 2007,
+    url: "https://www.goodreads.com/book/show/186094.The_Name_of_the_Wind"
+};
+
+
+console.log(name);
+console.log(edad);
+console.log(eresDeveloper);
+console.log(fechaDeNac);
+console.log(libroFavorito);
+
